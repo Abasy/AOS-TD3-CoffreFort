056dec5eccaf266ec7fe75cb452fba0a0f13a16d
Unknown <bastien-luck@hotmail.fr>
2019-04-08 13:44:39 +0200

    test yassin

--- acces_ressource_protegee/api_accees_ressources_proteger.py
+++ acces_ressource_protegee/api_accees_ressources_proteger.py
@@ -11,16 +11,17 @@ def api_apr():
     if ( test["token_coffre_fort"] is not None ) :
         port = "5578"
         context = zmq.Context()
-        socket = context.socket(zmq.PAIR)
+        socket = context.socket(zmq.REQ)
         socket.connect("tcp://localhost:%s" % port)
-        socket.send_string("from apr : " + test["token_coffre_fort"])
-        port = "5766"
-        socket = context.socket(zmq.PAIR)
+        socket.send_string("apr " + test["token_coffre_fort"])
+#        port = "5766"
+#        socket = context.socket(zmq.PAIR)
         socket.setsockopt(zmq.RCVTIMEO, 5000) #évite que le receive soit bloquant
         socket.connect("tcp://localhost:%s" % port)
         try : # necessaire car le timeout renvoi une erreur
             msg = socket.recv()
             msg = msg.decode("UTF-8")
+            print(msg)
         except :
             msg = "fail"
     if ( msg == "ok" ) :
